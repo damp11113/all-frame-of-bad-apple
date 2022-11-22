ac2a3c591f4e786f6138bf9e1dbded5d12614c97
damp pimsen <64675096+damp11113@users.noreply.github.com>
2022-11-22 19:19:05 +0700

    Update BadAppleAscii.py

--- BadAppleAscii.py
+++ BadAppleAscii.py
@@ -8,11 +8,11 @@ from damp11113.file import allfiles
 rx = 120
 ry = 70
 
-badapplefile = sort_files(allfiles(r'C:\Users\sansw\Documents\pythonProject\badappleframe'))
+badapplefile = sort_files(allfiles('badappleframe'))
 #size(rx, ry)
 
 for i in badapplefile:
     title(f'frame {i}/{len(badapplefile)}')
-    print(im2ascii(f'C:\\Users\\sansw\\Documents\\pythonProject\\badappleframe\\{i}', width=rx, height=ry))
+    print(im2ascii(f'badappleframe\\{i}', width=rx, height=ry))
     time.sleep(0.029)
     #clear()
